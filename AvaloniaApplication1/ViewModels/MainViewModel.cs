a13b34379e56344ea5da3f4d477f4ec63721f0c1
Giulio Ungaretti <giulioungaretti@me.com>
2024-02-25 13:43:34 +0100

    Use bleeding edge avalonia to fix inline bugs

--- AvaloniaApplication1/ViewModels/MainViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -12,24 +12,25 @@ public partial class MainViewModel : ViewModelBase
 
     [ObservableProperty]
     private InlineCollection sources = [
+    [ObservableProperty] private InlineCollection sources =
+    [
         new Run("Starting run"),
-        // TODO: having this inline container here breaks adding new inlines
-        new InlineUIContainer(new TextBox() { Text="Input", BorderThickness = Avalonia.Thickness.Parse("0"), }),
-        ];
 
     [RelayCommand]
     private void AddInline()
     {
         Sources.Add(new Run("Added run"));
+    [RelayCommand]
+    private void AddRun()
+    {
+        Sources.Add(new Run() { Text = "Run Text " });
         Sources.Add(new LineBreak());
-        // TODO: having this inline container here breaks adding new inlines
-        Sources.Add(new TextBox() { Text = "Input", BorderThickness = Avalonia.Thickness.Parse("0"), });
+    }
     }
 
     [RelayCommand]
     private void ClearInlines()
     {
-        //TODO: this PR  should fix ths https://github.com/AvaloniaUI/Avalonia/pull/14247
         Sources.Clear();
     }
 }
